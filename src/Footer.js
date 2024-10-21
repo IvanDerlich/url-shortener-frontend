8e02a892b09203b48d8564deccc82acc40ff8e36
Ivan Derlich <a@ivanderlich.com>
2024-10-21 17:56:37 -0300

    Add repos info

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,6 @@
-import React from "react";
 import styles from "./Footer.module.css"; // Import the CSS module
 
-function UrlShortener() {
+function Footer() {
   return (
     <footer className={styles.footer}>
       <p className={styles.creditsText}>
@@ -24,8 +23,38 @@ function UrlShortener() {
           GitHub
         </a>
       </p>
+      <div className={styles.repoLinks}>
+        <p>
+          <a
+            href="https://link-shortened-be-a8615336383d.herokuapp.com/api-docs"
+            target="_blank"
+            className={styles.repoLink}
+            rel="noreferrer"
+          >
+            Backend Docs
+          </a>{" "}
+          |
+          <a
+            href="https://github.com/IvanDerlich/linkshortener"
+            target="_blank"
+            className={styles.repoLink}
+            rel="noreferrer"
+          >
+            Backend Repo
+          </a>{" "}
+          |
+          <a
+            href="https://github.com/IvanDerlich/url-shortener-frontend"
+            target="_blank"
+            className={styles.repoLink}
+            rel="noreferrer"
+          >
+            Frontend Repo
+          </a>
+        </p>
+      </div>
     </footer>
   );
 }
 
-export default UrlShortener;
+export default Footer;
